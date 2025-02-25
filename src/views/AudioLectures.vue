899edbf871a259a1872cbe414467f7157d231bb6
lauvAri <198719993@qq.com>
2025-02-25 22:59:10 +0800

    display pdf summary

--- src/views/AudioLectures.vue
+++ src/views/AudioLectures.vue
@@ -1,33 +1,20 @@
 <template>
   <div class="pdf-viewer">
     <h1>PDF Viewer and Summarizer</h1>
-<!--    &lt;!&ndash; 文件上传 &ndash;&gt;-->
-<!--    <input type="file" accept="application/pdf" @change="handleFileUpload" />-->
-<!--    <VuePDF-->
-<!--      v-if="pdf"-->
-<!--      :pdf="pdf"-->
-<!--      text-layer-->
-<!--      annotation-layer-->
-<!--      :page="currentPage"-->
-<!--    />-->
-<!--    <div v-else class="placeholder">请上传一个 PDF 文件以查看内容。</div>-->
-
-<!--    <div class="controls" v-if="pdf">-->
-<!--      <v-btn @click="prevPage" :disabled="currentPage === 1">上一页</v-btn>-->
-<!--      <span> {{ currentPage }} / {{ pages }} </span>-->
-<!--      <v-btn @click="nextPage" :disabled="currentPage === pages">下一页</v-btn>-->
-<!--    </div>-->
     <input type="file" accept="application/pdf" @change="handleFileUpload" />
     <div id="pdf-info">
-      <v-btn v-on:click="fetchPdfPage(currentPage)">click me</v-btn>
-      <p>{{pdfPath}}</p>
-      <p>Total Pages: {{pages}}</p>
-      <p>Current Page: {{currentPage}}</p>
+      <v-btn v-on:click="fetchPdfPage(currentPage)">上传</v-btn>
+      <p>{{currentPage}} / {{ pages }}</p>
     </div>
-    <img v-if="imageData" :src="imageData" alt="PDF Page">
+    <img v-if="imageData" :src="imageData" alt="PDF Page" width="50%">
     <div class="controls" v-if="imageData">
       <v-btn @click="prevPage" :disabled="currentPage === 1">previous</v-btn>
       <v-btn @click="nextPage" :disabled="currentPage === pages">next</v-btn>
+      <v-btn @click="getSummary">summary</v-btn>
+    </div>
+    <div class="summary">
+      <p>{{ summary }}</p>
+      <v-btn>播放</v-btn>
     </div>
   </div>
 </template>
@@ -41,12 +28,13 @@ import { VPdfViewer } from '@vue-pdf-viewer/viewer'
 const flaskURL = 'http://localhost:5000';
 
 // 初始化状态
-const pdf = ref(null);
 const pages = ref(0);
 const currentPage = ref(1);
 const pdfPath = ref("upload/pdf/test.pdf");
 const imageData = ref(null);
+const summary = ref(null);
 
+// 获取某一页pdf的图片
 const fetchPdfPage = (pageNum) => {
   fetch(`${flaskURL}/api/get_pdf_page?pdf_path=${pdfPath.value}&page_num=${currentPage.value}`)
       .then(response => {
@@ -66,18 +54,6 @@ const fetchPdfPage = (pageNum) => {
       })
 }
 
-// // 动态加载 PDF
-// const loadPDF = async (fileURL) => {
-//   const { pdf: loadedPDF, pages: totalPages } = usePDF(fileURL, {
-//     onError: (error) => {
-//       console.error("Failed to load PDF:", error);
-//     }
-//   }); // 使用 usePDF 加载 PDF
-//   pdf.value = loadedPDF;
-//   pages.value = totalPages;
-//   currentPage.value = 1; // 重置当前页码
-// };
-//
 // 处理文件上传
 const handleFileUpload = (event) => {
   const file = event.target.files[0];
@@ -115,6 +91,44 @@ const nextPage = () => {
     fetchPdfPage(currentPage.value);
   }
 };
+
+const getSummary = () => {
+  fetch(`${flaskURL}/api/get_summary`, {
+    method: "POST",
+    body: JSON.stringify(
+      {
+        img_data: imageData.value
+      }
+    ),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Error: could not fetch summary");
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      summary.value = data.summary;
+    })
+    .catch(error => {
+      console.error("error fetching summary: ", error);
+    })
+  // fetch(`${flaskURL}/api/get_summary?pdf_path=${pdfPath.value}&page_num=${currentPage.value}`)
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error("Error: could not fetch summary");
+  //       }
+  //       return response.json();
+  //     })
+  //     .then(data => {
+  //       console.log("summary data: ", data);
+
+  //     })
+}
 </script>
 
 <style scoped>
@@ -132,5 +146,14 @@ const nextPage = () => {
 
 .controls {
   margin-top: 20px;
+}
+
+div.summary {
+  background: rgba(0, 0, 0, 0.05);
+  margin: 1rem;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  width: 50%;
+  transform: translateX(50%);
 }
 </style>
